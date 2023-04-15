09cf32a4df281f2a99151e6837397a21bbf1a4fb
saviorxzero98 <saviorxzero98@gmail.com>
2023-04-15 18:29:05 +0800

    update: twoSum solution

--- data_structure/hash/001-easy/solution.ts
+++ data_structure/hash/001-easy/solution.ts
@@ -1,4 +1,20 @@
 function twoSum(nums: number[], target: number): number[] {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return [
+                map.get(nums[i]),
+                i
+            ];
+        }
+        else {
+            map.set(target - nums[i], i);
+        }
+    }
+    return null;
+};
+
+function twoSum2(nums: number[], target: number): number[] {
     let indeies = [];
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
